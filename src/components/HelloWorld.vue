133ae70e350d1830ce1550a3c5f8014a2a332f50
Raywon <mrwjs0803@naver.com>
2020-06-09 17:06:59 +0900

    'setting'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,4 @@
-<template>
+<template v-for="item in items">
   <v-app id="inspire">
     <v-card height="100%">
       <v-toolbar flat>
@@ -14,15 +14,57 @@
           <v-icon>mdi-translate</v-icon>
         </v-btn>
 
-        <v-btn icon>
+        <v-btn icon @click="dialog1 = !dialog1">
           <v-icon>mdi-account</v-icon>
         </v-btn>
 
+        <v-dialog v-model="dialog1" width="600px">
+          <v-card>
+            <v-card-title>
+              My Profiles
+            </v-card-title>
+            <v-container>
+              <v-row class="mx-2">
+                <v-col class="align-center justify-space-between" cols="12">
+                  <v-row align="center" class="mr-0">
+                    <v-avatar size="40px" class="mx-3">
+                      <img src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png" alt="">
+                    </v-avatar>
+                    <v-text-field placeholder="Raywon"></v-text-field>
+                  </v-row>
+                </v-col>
+                <v-col cols="6">
+                  <v-text-field prepend-icon="mdi-account-card-details-outline" placeholder="Machine Learning Developer"></v-text-field>
+                </v-col>
+                <v-col cols="6">
+                  <v-text-field placeholder="Researcher"></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field prepend-icon="mdi-mail" placeholder="Email"></v-text-field>
+                </v-col>
+              </v-row>
+            </v-container>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn text @click="dialog1 = false; openurl(address[0]);">
+                <v-icon>mdi-youtube</v-icon>
+              </v-btn>
+              <v-btn text @click="dialog1 = false; openurl(address[1]);">
+                <v-icon>mdi-instagram</v-icon>
+              </v-btn>
+              <v-btn text @click="dialog1 = false; openurl(address[2]);">
+                <v-icon>mdi-github</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
 
-        <v-btn icon @click="dialog = !dialog">
+
+        <v-btn icon @click="dialog2 = !dialog2">
           <v-icon>mdi-email</v-icon>
         </v-btn>
-        <v-dialog v-model="dialog" width="800px">
+
+        <v-dialog v-model="dialog2" width="800px">
           <v-card>
             <v-card-title class="blue darken-2">
               Contact
@@ -55,10 +97,9 @@
               </v-row>
             </v-container>
             <v-card-actions>
-
               <v-spacer></v-spacer>
-              <v-btn text color="primary" @click="dialog = false">Cancel</v-btn>
-              <v-btn text @click="dialog = false">Sand</v-btn>
+              <v-btn text color="primary" @click="dialog2 = false">Cancel</v-btn>
+              <v-btn text @click="dialog2 = false">Sand</v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
@@ -69,19 +110,19 @@
         <template v-slot:extension>
           <v-tabs v-model="tabs" fixed-tabs>
             <v-tabs-slider></v-tabs-slider>
-            <v-tab href="#mobile-tabs-5-1" class="primary--text" >
+            <v-tab href="#mobile-tabs-1-1" class="primary--text">
               <v-icon>mdi-phone</v-icon>
             </v-tab>
 
-            <v-tab href="#mobile-tabs-5-2" class="primary--text" >
+            <v-tab href="#mobile-tabs-1-2" class="primary--text">
               <v-icon>mdi-heart</v-icon>
             </v-tab>
 
-            <v-tab href="#mobile-tabs-5-3" class="primary--text" >
+            <v-tab href="#mobile-tabs-1-3" class="primary--text">
               <v-icon>mdi-account-box</v-icon>
             </v-tab>
 
-            <v-tab href="#mobile-tabs-5-4" class="primary--text" >
+            <v-tab href="#mobile-tabs-1-4" class="primary--text">
               <v-icon>mdi-email</v-icon>
             </v-tab>
 
@@ -90,11 +131,26 @@
         </template>
       </v-toolbar>
 
-      <v-tabs-items v-model="tabs">
-        <v-tab-item v-for="i in 5" :key="i" :value="'mobile-tabs-5-' + i">
-          <v-card flat>
-            <v-card-text v-text="items"></v-card-text>
+      <v-tabs-items v-model="tabs" >
+        <v-tab-item v-for="i in 4" :key="i" :value="'mobile-tabs-1-' + i">
+
+          <v-card flat v-if="i == 1">
+            <v-card-text v-text="items[0].text"></v-card-text>
           </v-card>
+
+          <v-card flat  v-if="i == 2">
+            <v-card-text v-text="items[1]"></v-card-text>
+          </v-card>
+
+          <v-card flat  v-if="i == 3">
+            <v-card-text v-text="items[2]"></v-card-text>
+          </v-card>
+
+          <v-card flat  v-if="i == 4">
+            <v-card-text v-text="items[3]"></v-card-text>
+          </v-card>
+
+
         </v-tab-item>
       </v-tabs-items>
     </v-card>
@@ -124,16 +180,29 @@
     data: () => ({
       tabs: null,
       page: [1, 2, 3, 4],
-      text:"ssibal",
-      dialog: false,
+      text: "ssibal",
+      dialog1: false,
+      dialog2: false,
+      address: [
+        'https://www.youtube.com/mrwjs0803',
+        'https://www.instagram.com/ray._.won',
+        'https://www.github.com/raywon'],
+
       drawer: null,
       items: [
-      {text: '영범이 고추 3cm', age : '14살'},
-      {text: '장현이 고추 3cm'},
-      {text: '용진이 고추 3cm'},
-      {text: 'ㄱㅎ'}
+        { text: '영범이 고추 3cm', age: '14살' },
+        { text: '장현이 고추 3cm' },
+        { text: '용진이 고추 3cm' },
+        { text: 'ㄱㅎ' }
 
       ],
     }),
+    methods: {
+      openurl(url){
+        window.open(url)
+
+      }
+
+    }
   }
 </script>
